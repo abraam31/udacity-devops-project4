726234aa18c5567eac95af0723f845023d5e434a
Ubuntu <ubuntu@ip-172-31-43-90.eu-west-1.compute.internal>
2021-01-04 15:19:33 +0000

    docker push

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,13 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=abraam31/udacity-sklearn:latest
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
+cat temp/password.txt | docker login --username abraam31 --password-stdin
+docker tag udacity-sklearn $dockerpath
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
